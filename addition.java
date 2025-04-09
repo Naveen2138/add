7a0c248116f2f7c8402a05d2cf727ce5191e1302
Naveen2138 <naveenkumarvajrad04@gmail.com>
2025-04-09 15:03:06 +0530

    java code

--- addition.java
+++ addition.java
@@ -2,7 +2,7 @@
 public class addition {
     public static void main(String[] args){
         int a=10,b=5;
-        int sum=a+b;
-        System.out.println("The sum is:"+sum);
+        int sub=a-b;
+        System.out.println("The sub is:"+sub);
     }
 }
